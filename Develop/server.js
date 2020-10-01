0811873cc07f17c9d862dd524fb8c03193aa6c9d
Lauren Brown <laurenbrown108@gmail.com>
2020-10-01 12:43:00 -0400

    I think it's reading the json file correctly.

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,12 @@
+const { notStrictEqual } = require("assert");
 //Dependencies
 const express = require("express");
+const { readFile, fstat } = require("fs");
 const path = require("path");
 const db = require("./db/db.json");
+const fs = require("fs");
+
+let notes = [];
 
 //Setting up Express
 const app = express();
@@ -10,6 +15,7 @@ const PORT = process.env.PORT || 3000;
 //Setting up Express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "./public")));
 
 //Routes
 app.get("/", function(req, res) {
@@ -19,7 +25,21 @@ app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"))
 });
 
-//post
+//Display notes
+app.get("/api/notes", function(req, res){
+    notes = fs.readFileSync("./db/db.json", "UTF8", err => {
+        if (err)
+        throw (err)
+    })
+    notes = JSON.parse(notes);
+    res.json(notes);
+    console.log(notes);
+});
+
+//Post notes
+//app.post("/api/notes", function(req,))
+
+
 //writetofile include db in fs.writeFile()
 //i think you have to do this when saving the "deleted version too"
 
